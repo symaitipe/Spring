cc6e5e65b2a09dcd2fc475300526001795a05256
sahan yasas maitipe <sahanmaitipe2002@gmail.com>
2025-02-16 11:17:16 +0530

    feat: Add getAllProducts endpoint with empty list handling

--- spring-ecommerce/spring-ecommerce/src/main/java/com/sahan/spring_ecommerce/controller/ProductController.java
+++ spring-ecommerce/spring-ecommerce/src/main/java/com/sahan/spring_ecommerce/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.sahan.spring_ecommerce.controller;
 import com.sahan.spring_ecommerce.model.Product;
 import com.sahan.spring_ecommerce.service.ProductServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +23,13 @@ public class ProductController {
 
 
     @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        //System.out.println("Get all products called");
-        return services.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        System.out.println("Get all products called");
+        List<Product> allProductsList = services.getAllProducts();
+        if(allProductsList.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(allProductsList,HttpStatus.OK);
     }
 
 
